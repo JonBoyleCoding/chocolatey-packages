f3461114d9352d8b3881dcd4e404f3de5aaa0c56
Chocolatey Auto Updater <chocolatey@realdimensions.net>
2019-04-14 00:01:18 +0000

    AU: 1 updated - universal-ctags [skip ci] https://gist.github.com/50ac4a9d98c2d1f86d625e8b55b84d2a/ef7527694420b13ef86e281da24225fbf7619fa9

--- automatic/universal-ctags/tools/chocolateyInstall.ps1
+++ automatic/universal-ctags/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion
 
 $packageArgs = @{
   packageName    = 'universal-ctags'
-  url            = 'https://github.com/universal-ctags/ctags-win32/releases/download/2019-04-12/f07b0a98/ctags-2019-04-12_f07b0a98-x86.zip'
-  url64Bit       = 'https://github.com/universal-ctags/ctags-win32/releases/download/2019-04-12/f07b0a98/ctags-2019-04-12_f07b0a98-x64.zip'
-  checksum       = 'FAE48C1B12028315A344038AA0BBBB2ECF4FB4104F4C81D7920B83C1E36EF9F4'
-  checksum64     = '85FA432EFA675C729DE0EA47DDDFD15C128100D3E138C45101C970BEC94448BD'
+  url            = 'https://github.com/universal-ctags/ctags-win32/releases/download/2019-04-13/61cc66cf/ctags-2019-04-13_61cc66cf-x86.zip'
+  url64Bit       = 'https://github.com/universal-ctags/ctags-win32/releases/download/2019-04-13/61cc66cf/ctags-2019-04-13_61cc66cf-x64.zip'
+  checksum       = '22532C195A369E6F441845BA0BD8B7E4D9B626A2A418019B8DC207A85764D0BE'
+  checksum64     = '318EB8F1B1802AFF343E207E931C28D9AA025CEF2DE2F86F580B846329B0BC50'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $toolsPath
